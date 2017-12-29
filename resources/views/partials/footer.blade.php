3333c42e30e0e976cddb934bd4765f233abcf0a0
Frank Siderio <frank.siderio1@marist.edu>
2017-12-29 00:34:44 -0500

    home is more mobile friendly, adding images to footer

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -2,15 +2,28 @@
 <footer class="footer col-md-12">
     <div class="col-md-6">
         <h4>Site Map</h4>
+        <a href="/">Home</a><br>
         <a href="">Work Experience</a><br>
         <a href="">Projects</a><br>
-        <a href="">Skills</a>
+        <a href="">Skills</a><br>
+        <a href="/contact">Contact</a><br>
     </div>
     <div class="col-md-6">
         <h4>This site was created with:</h4>
-        <a href="">Work Experience</a><br>
-        <a href="">Projects</a><br>
-        <a href="">Skills</a>
+        <a href=""><img class="footerImage" src="../images/footer/aws.png"/></a>
+        <a href=""><img class="footerImage" src="../images/footer/postgres.png"/></a><br>
+        <a href=""><img class="footerImage" src="../images/footer/adonis.png"/></a>
+        <a href=""><img class="footerImage" src="../images/footer/react.png"/></a>
+        <a href=""><img class="footerImage" src="../images/footer/laravel.png"/></a>
     </div>
     <p>Copyright  {{ \Carbon\Carbon::now()->year }}  © Frank Siderio All rights reserved.</p>
 </footer>
+
+<style>
+
+.footerImage {
+    width: 100px;
+    height: 100px;
+}
+
+</style>
