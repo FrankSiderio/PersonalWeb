b4721fffa43bf4b4eda8f4f9a14e0d23458bf589
Frank Siderio <frank.siderio1@marist.edu>
2017-12-29 00:39:44 -0500

    a lil cleanup

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -10,20 +10,11 @@
     </div>
     <div class="col-md-6">
         <h4>This site was created with:</h4>
-        <a href=""><img class="footerImage" src="../images/footer/aws.png"/></a>
-        <a href=""><img class="footerImage" src="../images/footer/postgres.png"/></a><br>
-        <a href=""><img class="footerImage" src="../images/footer/adonis.png"/></a>
-        <a href=""><img class="footerImage" src="../images/footer/react.png"/></a>
-        <a href=""><img class="footerImage" src="../images/footer/laravel.png"/></a>
+        <a href=""><img class="footer-image" src="../images/footer/aws.png"/></a>
+        <a href=""><img class="footer-image" src="../images/footer/postgres.png"/></a><br>
+        <a href=""><img class="footer-image" src="../images/footer/adonis.png"/></a>
+        <a href=""><img class="footer-image" src="../images/footer/react.png"/></a>
+        <a href=""><img class="footer-image" src="../images/footer/laravel.png"/></a>
     </div>
     <p>Copyright  {{ \Carbon\Carbon::now()->year }}  © Frank Siderio All rights reserved.</p>
 </footer>
-
-<style>
-
-.footerImage {
-    width: 100px;
-    height: 100px;
-}
-
-</style>
