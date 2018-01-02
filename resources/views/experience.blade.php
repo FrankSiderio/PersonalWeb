de6efa0336c42b2957f69e514ac168ddf3efff0b
Frank Siderio <frank.siderio1@marist.edu>
2018-01-02 12:55:34 -0500

    finishing up experience page

--- resources/views/experience.blade.php
+++ resources/views/experience.blade.php
@@ -1,5 +1,10 @@
 @extends('layout')
     <title>Frank Siderio - Experience</title>
+
+@section('css')
+    <link href="{{ asset('css/experience.css') }}" rel="stylesheet" type="text/css">
+@endsection
+
 @section('content')
 
     <div class="hero-section">
@@ -16,105 +21,98 @@
         <h2 class="section-text">My work experience</h2>
     </div>
 
-    <div id="Wawa Inc" class="container section">
-        <div class="left-right">
-            <div class="content">
-                <h2 class="name">Developer at Wawa</h2>
-                <p>Description text jfkdsl fdjsklf djskfld jskfldsj fksldjfk.</p>
-            </div>
+    <div id="Wawa Inc" class="section">
+        <div class="container">
+            <div class="left-right">
+                <div class="content">
+                    <h2 class="name">Developer at Wawa</h2>
+                    <p>Description text jfkdsl fdjsklf djskfld jskfldsj fksldjfk fksldjfkfksldjfk fksldjfk fksldjfk fksldjfk fksldjfk fksldjfk fksldjfk.</p>
+                </div>
 
-            <div>
-                <img class="img-responsive" src="../images/experience/wawa_mobile.png"/>
-            </div>
-        </div>
-    </div><br></br>
-    <div id="Marist SGA" class="container section">
-        <div class="left-right">
-            <div class="content">
-                <h2 class="name">CIO at Marist SGA</h2>
-                <p>Description text jfkdsl fdjsklf djskfld jskfldsj fksldjfk.</p>
-            </div>
-
-            <div>
-                <img class="img-responsive" src="../images/experience/sga.png"/>
-            </div>
-        </div>
-    </div><br></br>
-    <div id="iD Tech" class="container section">
-        <div class="left-right">
-            <div class="content">
-                <h2 class="name">Instructor at iD Tech</h2>
-                <p>Description text jfkdsl fdjsklf djskfld jskfldsj fksldjfk.</p>
-            </div>
-
-            <div>
-                <img class="img-responsive" src="../images/experience/id_tech.jpg"/>
+                <div class="image-container">
+                    <img class="img-responsive" src="../images/experience/wawa_mobile.png"/>
+                </div>
             </div>
         </div>
     </div>
+    <div id="Marist SGA" class="dark-section">
+        <div class="container">
+            <div class="left-right">
+                <div class="content">
+                    <h2 class="name">CIO at Marist SGA</h2>
+                    <p>Description text jfkdsl fdjsklf djskfld jskfldsj fksldjfk.</p>
+                </div>
+
+                <div class="image-container">
+                    <img class="img-responsive" src="../images/experience/sga.png"/>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div id="iD Tech" class="section">
+        <div class="container">
+            <div class="left-right">
+                <div class="content">
+                    <h2 class="name">Instructor at iD Tech</h2>
+                    <p>Description text jfkdsl fdjsklf djskfld jskfldsj fksldjfk.</p>
+                </div>
+
+                <div class="image-container">
+                    <img class="img-responsive" src="../images/experience/id_tech.png"/>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="heading">
+        <h2 class="section-text">My projects</h2>
+    </div>
+
+    <div id="Operating System" class="section">
+        <div class="container">
+            <div class="left-right">
+                <div class="content">
+                    <h2 class="name">Operating System</h2>
+                    <p>Description text jfkdsl fdjsklf djskfld jskfldsj fksldjfk fksldjfkfksldjfk fksldjfk fksldjfk fksldjfk fksldjfk fksldjfk fksldjfk.</p>
+                </div>
+
+                <div class="image-container">
+                    <img class="img-responsive" src="../images/experience/os.png"/>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div id="Capping" class="dark-section">
+        <div class="container">
+            <div class="left-right">
+                <div class="content">
+                    <h2 class="name">Capping</h2>
+                    <p>Description text jfkdsl fdjsklf djskfld jskfldsj fksldjfk.</p>
+                </div>
+
+                <div class="image-container">
+                    <img class="img-responsive" src="../images/experience/capping.png"/>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div id="MLB Scoreboard" class="section">
+        <div class="container">
+            <div class="left-right">
+                <div class="content">
+                    <h2 class="name">MLB Scoreboard</h2>
+                    <p>Description text jfkdsl fdjsklf djskfld jskfldsj fksldjfk.</p>
+                </div>
+
+                <div class="image-container">
+                    <img class="img-responsive" src="../images/experience/arduino.png"/>
+                </div>
+            </div>
+        </div>
+    </div>
+
     <hr></hr>
 @endsection
-
-<style>
-
-.hero-text {
-    color: white;
-    position: absolute;
-    top: 50%;
-    left: 50%;
-    transform: translate(-50%, -50%);
-}
-
-.container-fluid {
-   padding: 0px;
-}
-
-.left-right {
-    display: grid;
-
-    grid-template-columns: repeat(2, 1fr);
-    grid-gap: 20px;
-}
-
-@media (max-width: 400px) {
-    .left-right {
-        grid-template-columns: repeat(1, 1fr);
-    }
-}
-
-.content {
-    text-align: center;
-    margin-top: 100px;
-}
-
-@media (max-width: 950px) {
-    .content {
-        margin-top: 60px;
-    }
-}
-
-@media (max-width: 600px) {
-    .content {
-        margin-top: 35px;
-    }
-}
-
-@media (max-width: 545px) {
-    .content {
-        margin-top: 0px;
-    }
-}
-
-.section {
-    box-shadow: 0 10px 10px lightgray;
-
-    background-color: #eeeeee;
-
-    padding: 10px;
-}
-
-</style>
-
 
 @section('js')
     <script src="{{asset('js/app.js')}}" ></script>
