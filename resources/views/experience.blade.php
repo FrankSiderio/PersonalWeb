16190a9f130d86704a8b2bb9cfc59409b925a1ce
Frank Siderio <frank.siderio1@marist.edu>
2018-01-04 16:01:26 -0500

    updating text

--- resources/views/experience.blade.php
+++ resources/views/experience.blade.php
@@ -27,7 +27,7 @@
             <div class="left-right">
                 <div class="content">
                     <h2 class="name">Developer at Wawa</h2>
-                    <p>The past two summers I was on the mobile app team. My first summer I worked on mainly supporting the app. I created test scripts in Postman to test mobile ordering and worked a lot in MySql. My second summer I worked with developers on features and bug fixes. I even was apart of creating a test app for geofence testing. I gained experience with Postman, MySql, AWS, Android Studio, APIs, Java, and Retrofit.</p>
+                    <p>For the past two summers, I was on the mobile app team. During my first summer, I worked on mainly supporting the app. I created test scripts in Postman to test mobile ordering and worked a lot in MySql. My second summer, I worked with developers on features and bug fixes. I was part of creating a test app for geofence testing. I gained experience with Postman, MySql, AWS, Android Studio, APIs, Java, and Retrofit.</p>
                     <div class="button-container">
                         <a class="button btn-component" target="_blank" href="https://play.google.com/store/apps/details?id=com.wawa.android.appv2&hl=en">See Work</a>
                     </div>
@@ -44,7 +44,7 @@
             <div class="left-right">
                 <div class="content">
                     <h2 class="name">CIO at Marist SGA</h2>
-                    <p>As the liason between the student body and Marist IT it is one of my duties to handle technology issues on campus. I lead a team who are responsible for improving and supporting the software applications for SGA and Student Activities. We support applications like our club management system, club websites, parking appeals, and our website. My team has developers, QA Testers, and Designers. We work with Linux, Apache, MySql, and PHP.</p>
+                    <p>As the liason between the student body and Marist IT, it is one of my duties to handle technology issues on campus. I lead a team who is responsible for improving and supporting the software applications for SGA and Student Activities. We support applications like our club management system, club websites, parking appeals, and our website. My team has developers, QA Testers, and Designers. We work with Linux, Apache, MySql, and PHP.</p>
                     <div class="button-container">
                         <a class="button btn-component" target="_blank" href="https://sga.marist.edu">See Work</a>
                     </div>
@@ -61,7 +61,7 @@
             <div class="left-right">
                 <div class="content">
                     <h2 class="name">Instructor at iD Tech</h2>
-                    <p>As an instructor at iD Tech I was able to teach kids about technology. I taught classes on how to program with JavaScript. Creating different skins and models in minecraft. And my favorite, I taught a Minecraft modding class.</p>
+                    <p>As an instructor at iD Tech, I was able to teach kids about technology. I taught classes on how to program with JavaScript. As well as, a class in creating different skins and models in Minecraft. My favorite class was teaching Minecraft modding with Java.</p>
                     <div class="button-container">
                         <a class="button btn-component" target="_blank" href="https://www.idtech.com/">See Work</a>
                     </div>
@@ -85,7 +85,7 @@
             <div class="left-right">
                 <div class="content">
                     <h2 class="name">Operating System</h2>
-                    <p>This project was created throughout my fall 2016 semester in my Operating Systems class. We learned how OS's worked and we got to build one ourselves. My OS can run hex programs, write, read, and delete files, and can run multiple programs at once. I used TypeScript that compiles into JavaScript to create the OS.</p>
+                    <p>This project was created throughout my Fall 2016 semester in my Operating Systems class. We learned how OS's worked and we got to build one ourselves. My OS can run hex programs, write, read, and delete files, and can run multiple programs at once. I used TypeScript that compiles into JavaScript to create the OS.</p>
                     <div class="button-container">
                         <a class="button btn-component" target="_blank" href="http://foxweb.marist.edu/users/frank.siderio1/">See Project</a>
                     </div>
